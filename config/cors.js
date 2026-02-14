1bfd990ce023682e5b2655df82cfe27b8f8105f4
karsiman <karsy89@gmail.com>
2026-02-14 20:25:34 +0700

    flexible cors

--- config/cors.js
+++ config/cors.js
@@ -2,8 +2,22 @@
 
 const cors = require("cors");
 
+const allowedOrigins = process.env.CORS_ORIGIN
+  ? process.env.CORS_ORIGIN.split(",")
+  : [];
+
 module.exports = cors({
-  origin: "http://localhost:3000",
+  origin: function (origin, callback) {
+    // allow requests with no origin (mobile apps, postman, curl)
+    if (!origin) return callback(null, true);
+
+    if (allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization", "X-Auth-Irs"],
