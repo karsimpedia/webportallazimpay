37ef15a5a3e5e8e83bd157a9b0c7c5dd0f6d3dec
karsiman <karsy89@gmail.com>
2026-02-15 21:15:57 +0700

    update

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 const USE_HTTPS = process.env.USE_HTTPS === "true";
-
+app.set("trust proxy", 1);
 if (USE_HTTPS) {
   const credentials = {
     key: fs.readFileSync("./ssl/private.key"),
