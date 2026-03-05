7cb10d192922175056c8195499b8b49c72b2d4cc
karsiman <karsy89@gmail.com>
2026-03-05 08:42:31 +0700

    update

--- controllers/receiptTemplate.controller.js
+++ controllers/receiptTemplate.controller.js
@@ -428,38 +428,38 @@ exports.getReceiptByTransaction = async (req, res) => {
     };
 
     let sn = trx.serial;
-    var strukPLN =
-      "STRUK PEMBELIAN PLN PRABAYAR\n\n" +
-      "\nWaktu       :" +
-      formatDateSafe(trx.createdAt) +
-      "\nNo.METER    :" +
-      trx.msisdn +
-      "\nNAMA        :" +
-      sn.split("/")[1] +
-      "\nTD          :" +
-      sn.split("/")[2] +
-      "/" +
-      sn.split("/")[3] +
-      "\nKWH         :" +
-      sn.split("/")[4] +
-      //"\nKWH         :" + sn.split("/KWH:")[1].split("/")[0] +
+    // var strukPLN =
+    //   "STRUK PEMBELIAN PLN PRABAYAR\n\n" +
+    //   "\nWaktu       :" +
+    //   formatDateSafe(trx.createdAt) +
+    //   "\nNo.METER    :" +
+    //   trx.msisdn +
+    //   "\nNAMA        :" +
+    //   sn.split("/")[1] +
+    //   "\nTD          :" +
+    //   sn.split("/")[2] +
+    //   "/" +
+    //   sn.split("/")[3] +
+    //   "\nKWH         :" +
+    //   sn.split("/")[4] +
+    //   //"\nKWH         :" + sn.split("/KWH:")[1].split("/")[0] +
 
-      //"\nMATERRAI    :RP " + pad(utilirs.todesimal(materai).toString(), 12, { lpad: "." }) +
-      "\nNOMINAL     :Rp " +
-      pad(utilirs.todesimal(amountDueNum).toString(), 12, { lpad: "." }) +
-      "\nJASA LOKET  :Rp " +
-      pad(utilirs.todesimal(jasaloket).toString(), 12, { lpad: "." }) +
-      "\nBAYAR       :Rp " +
-      pad(utilirs.todesimal(totalFinal).toString(), 12, { lpad: "." }) +
-      "\n\n        STROOM/TOKEN " +
-      "\n   " +
-      sn.split("/")[0] +      
+    //   //"\nMATERRAI    :RP " + pad(utilirs.todesimal(materai).toString(), 12, { lpad: "." }) +
+    //   "\nNOMINAL     :Rp " +
+    //   pad(utilirs.todesimal(amountDueNum).toString(), 12, { lpad: "." }) +
+    //   "\nJASA LOKET  :Rp " +
+    //   pad(utilirs.todesimal(jasaloket).toString(), 12, { lpad: "." }) +
+    //   "\nBAYAR       :Rp " +
+    //   pad(utilirs.todesimal(totalFinal).toString(), 12, { lpad: "." }) +
+    //   "\n\n        STROOM/TOKEN " +
+    //   "\n   " +
+    //   sn.split("/")[0] +      
       
-      "\n\nStruk ini merupakan bukti" +
-      "\npembayaran yang sah" +
-      "\nLebih lanjut hubungi PLN 123\n" +
-      "\nTERIMA KASIH\r\n" +
-      "\n\nLazimPay - https://lazimpay.com\r\n";
+    //   "\n\nStruk ini merupakan bukti" +
+    //   "\npembayaran yang sah" +
+    //   "\nLebih lanjut hubungi PLN 123\n" +
+    //   "\nTERIMA KASIH\r\n" +
+    //   "\n\nLazimPay - https://lazimpay.com\r\n";
 
 
 
